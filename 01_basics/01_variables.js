96fbea56740d7893e4813bc3a5e8a3e7b6640ee7
Atharv907 <133625818+Atharv907@users.noreply.github.com>
2025-08-02 06:39:40 +0000

    Variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 144553
+let accountEmail = "atharva@google.com"
+var accountPassword = "12345"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as never version
+
+alert (3+3); //we are using nodejs, not browser
+let name ="atharva"
+let age= 22
+let isLoggedIn = true
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof "atharva");
