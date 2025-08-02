b4e540ede57b193c44c6c4f38d5847bc88ac22db
Atharv907 <133625818+Atharv907@users.noreply.github.com>
2025-08-02 15:08:21 +0000

    maths and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,25 @@
+// const score = 400
+// const balance = new Number (100)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// const  otherNumber = 223.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//-----------------------------Maths---------------------//
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(44.6));
+// console.log(Math.min(4, 3, 6, 1));
+// console.log(Math.max(4, 3, 6, 1));
+
+// console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
