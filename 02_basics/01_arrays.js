c030b5d18b77db816a9f4d954917b3967624f5d0
Atharv907 <133625818+Atharv907@users.noreply.github.com>
2025-08-02 21:40:14 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,19 @@
+//array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+
+
+console.log(myArr[0]);
+
+//Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+
+console.log(   myArr.includes(9));
+  
+console.log(myArr);
+
