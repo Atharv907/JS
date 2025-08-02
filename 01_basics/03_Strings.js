94c65e2f96f46b9368758831a78ee572fa459a63
Atharv907 <133625818+Atharv907@users.noreply.github.com>
2025-08-02 14:34:42 +0000

    strings

---
+++ 01_basics/03_Strings.js
@@ -0,0 +1,11 @@
+const url = "https://atharva.com/atharva%20chougale"
+console.log(url.replace('%20','-'))
+console.log(url.includes('Atharva'))
+
+const gameName = new String ('atharva-ac-com')
+console.log(gameName.split('-'))
+
+const name = "Atharva"
+const repoCount = 5
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
