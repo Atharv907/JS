386eeba79caa0c3c2b984730d6c01a3e42e024bd
Atharv907 <133625818+Atharv907@users.noreply.github.com>
2025-08-04 21:20:58 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,15 @@
+let myArray = ["car", "home", "bike", "watch"]
+let arr = 0
+while (arr < myArray.length){
+    console.log(myArray[arr]);
+    arr++
+}
+
+
+
+let score =1
+
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score<=10);
